105efcd4fa3e7b6084870b1500f6161652e30117
Rekkin99 <farrellbagoes04@gmail.com>
2025-11-30 20:18:31 +0700

    Inisiasi Kerangka Event Discovery

---
+++ lib/features/event-discovery/screens/event_discovery_page.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:bond_up_mobile/core/design_system.dart';
+import 'package:bond_up_mobile/features/event-discovery/data/models/event_model.dart';
+import 'package:bond_up_mobile/main.dart';
