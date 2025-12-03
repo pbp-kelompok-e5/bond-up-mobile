41d8ea805748cbb0761d58337d37d31c84f39c4d
ariefsolomon <ariefsolomon8@gmail.com>
2025-12-03 13:39:12 +0700

    save state

--- lib/features/event-management/screens/event_detail_page.dart
+++ lib/features/event-management/screens/event_detail_page.dart
@@ -1,4 +1,3 @@
-// lib/features/event_management/presentation/event_detail_page.dart
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
